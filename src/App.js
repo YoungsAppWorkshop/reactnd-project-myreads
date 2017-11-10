e603b5fcc36e2dcd46f9e4e352ee4be0297b49ee
Young Jung <ryviusm6@gmail.com>
2017-11-10 16:51:39 +0900

    feat: Fetch books list from the server

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {}
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+
+      /**
+      /* TODO: Remove log statement after refactoring code
+      **/
+      console.log(this.state.books)
+    })
+  }
 
   render() {
     return (
       <div className="app">
 
         <Route exact path='/' render={() => (
-          <ListBooks />
+          <ListBooks
+            books={this.state.books}
+          />
         )}/>
 
         <Route path='/search' render={() => (
